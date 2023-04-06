9f2df313849eaeb3e7a158aaa8b612a393795b00
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-04-06 17:04:55 -0400

    refactor: update test to pull tags from common dev yaml

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,17 +2,42 @@
 package test
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"gopkg.in/yaml.v3"
 )
 
 // Use existing resource group for tests
 const resourceGroup = "geretain-test-key-protect"
 const terraformDir = "examples/default"
 
-var accessTags = []string{"geretain-dev:permanent-test-tag-1", "geretain-dev:permanent-test-tag-2"}
+// Define a struct with fields that match the structure of the YAML data
+const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
+
+type Config struct {
+	ExistingAccessTags []string `yaml:"accessTags"`
+}
+
+func TestMain(m *testing.M) {
+	// Read the YAML file contents
+	data, err := os.ReadFile(yamlLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create a struct to hold the YAML data
+	var config Config
+	// Unmarshal the YAML data into the struct
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(m.Run())
+}
 
 func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
@@ -21,7 +46,7 @@ func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
 		TerraformVars: map[string]interface{}{
-			"access_tags": accessTags,
+			"access_tags": config.ExistingAccessTags,
 		},
 	})
 
