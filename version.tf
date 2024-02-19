1ec976f02fbe822215f6c3b833fe1b819f966e26
Md Anam Raihan <md.anam.raihan17@gmail.com>
2024-02-19 21:44:12 +0530

    fix: updated required provider version constraints to prevent major version updates (#526)
    Co-authored-by: Md Anam Raihan <Md.Anam.Raihan@ibm.com>

--- version.tf
+++ version.tf
@@ -4,7 +4,7 @@ terraform {
     # Use "greater than or equal to" range in modules
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.56.1"
+      version = ">= 1.56.1, < 2.0.0"
     }
   }
 }
