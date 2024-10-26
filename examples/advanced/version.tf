dca468dcaabc2706690a3d839b05aa2f72d9146e
shemau <shemau@gmail.com>
2024-10-26 19:04:55 +0100

    fix: bump provider to support CBR

--- examples/advanced/version.tf
+++ examples/advanced/version.tf
@@ -5,7 +5,7 @@ terraform {
     # module's version.tf (basic example), and 1 example that will always use the latest provider version (advanced example).
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.65.0"
+      version = ">= 1.70.0"
     }
   }
 }
