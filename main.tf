d200de8df00084a8d84638485975cdb20816c964
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-09 05:22:27 +0000

    fix(deps): update dependencies

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ locals {
 module "cbr_rule" {
   count            = length(var.cbr_rules)
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module"
-  version          = "1.32.6"
+  version          = "1.32.7"
   rule_description = var.cbr_rules[count.index].description
   enforcement_mode = var.cbr_rules[count.index].enforcement_mode
   rule_contexts    = var.cbr_rules[count.index].rule_contexts
