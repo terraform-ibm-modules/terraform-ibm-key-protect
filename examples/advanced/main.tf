b15c1b7a94c868f633fa395a628d700709f61628
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-06 14:04:51 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.1"
+  version = "1.4.2"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -71,7 +71,7 @@ module "key_protect_module" {
 
 module "kms_key_ring" {
   source        = "terraform-ibm-modules/kms-key-ring/ibm"
-  version       = "2.6.2"
+  version       = "2.6.3"
   instance_id   = module.key_protect_module.key_protect_guid
   key_ring_id   = "${var.prefix}-my-key-ring"
   endpoint_type = "private"
@@ -83,7 +83,7 @@ module "kms_key_ring" {
 
 module "ibm_kms_key" {
   source          = "terraform-ibm-modules/kms-key/ibm"
-  version         = "1.4.3"
+  version         = "1.4.4"
   kms_instance_id = module.key_protect_module.key_protect_guid
   key_name        = "${var.prefix}-root-key"
   kms_key_ring_id = module.kms_key_ring.key_ring_id
