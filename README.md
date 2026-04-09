18d8f00a91bfb327202fc26618c49d566317747a
Piyush Kumar Sahu <abcpiyush7@gmail.com>
2026-04-09 22:51:50 +0530

    feat(deps): updating module provider range strategy to support IBM provider v2 (#801)

--- README.md
+++ README.md
@@ -65,13 +65,13 @@ To attach access management tags to resources in this module, you need the follo
 | Name | Version |
 |------|---------|
 | <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.9.0 |
-| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.79.0, < 2.0.0 |
+| <a name="requirement_ibm"></a> [ibm](#requirement\_ibm) | >= 1.79.0, < 3.0.0 |
 
 ### Modules
 
 | Name | Source | Version |
 |------|--------|---------|
-| <a name="module_cbr_rule"></a> [cbr\_rule](#module\_cbr\_rule) | terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module | 1.35.19 |
+| <a name="module_cbr_rule"></a> [cbr\_rule](#module\_cbr\_rule) | terraform-ibm-modules/cbr/ibm//modules/cbr-rule-module | 1.36.0 |
 
 ### Resources
 
--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.8"
+  version = "1.6.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -24,7 +24,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 # A network zone with Service reference to schematics
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.35.19"
+  version          = "1.36.0"
   name             = "${var.prefix}-network-zone"
   zone_description = "CBR Network zone for schematics"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -49,7 +49,7 @@ module "key_protect_module" {
   tags              = var.resource_tags
   access_tags       = var.access_tags
   allowed_network   = "private-only"
-  # CBR rule only allowing the Key Protect instance to be accessbile from Schematics
+  # CBR rule only allowing the Key Protect instance to be accessible from Schematics
   cbr_rules = [{
     description      = "${var.prefix}-key-protect access only from schematics"
     enforcement_mode = "enabled"
@@ -71,7 +71,7 @@ module "key_protect_module" {
 
 module "kms_key_ring" {
   source        = "terraform-ibm-modules/kms-key-ring/ibm"
-  version       = "2.6.29"
+  version       = "2.7.0"
   instance_id   = module.key_protect_module.key_protect_guid
   key_ring_id   = "${var.prefix}-my-key-ring"
   endpoint_type = "private"
@@ -83,7 +83,7 @@ module "kms_key_ring" {
 
 module "ibm_kms_key" {
   source          = "terraform-ibm-modules/kms-key/ibm"
-  version         = "1.4.26"
+  version         = "1.5.0"
   kms_instance_id = module.key_protect_module.key_protect_guid
   key_name        = "${var.prefix}-root-key"
   kms_key_ring_id = module.kms_key_ring.key_ring_id
